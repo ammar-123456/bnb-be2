31a224f08ff7aa186db3a56f007ef08041037bce
Ammar <ammari11alhalabi@gmail.com>
2025-04-27 03:04:25 +0200

    kod

--- lib/mock-auth.ts
+++ lib/mock-auth.ts
@@ -1,37 +1,3 @@
-// "use client";
-
-// export const UserButton = () => {
-//   // return <button>Login</button>;
-// };
-
-// export const useAuth = () => {
-//   return {
-//     isSignedIn: true,
-//     userId: "mock-user-123",
-//   };
-// };
-
-// export const useUser = () => {
-//   return {
-//     user: {
-//       id: "mock-user-123",
-//       firstName: "Mock",
-//       lastName: "User",
-//       emailAddresses: [{ emailAddress: "mock@example.com" }],
-//     },
-//     isLoaded: true,
-//     isSignedIn: true,
-//   };
-// };
-
-// export const ClerkProvider = ({ children }: { children: React.ReactNode }) => {
-//   // return <>{children}</>;
-// };
-
-
-
-
-
 "use client";
 
 import { ReactNode } from "react";
